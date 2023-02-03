505eb588a0717f06ea6bbd128e235cbf3e7712d5
IsmaelMartins934 <ismaelmartins934@gmail.com>
2023-02-03 13:13:25 -0300

    a

--- main.js
+++ main.js
@@ -4,11 +4,30 @@ for(i in btn){
     btn[i].addEventListener('click', (e) => {
         e.this
         const path = e.composedPath()
-        console.log(path[1])
         page(path[1])
     })
 }
 
 function page(item){
-    
+    const fone = document.querySelector('div.texto-fone')
+    const celular_promocao = document.querySelector('div.texto-prom')
+    const mais_vendidos = document.querySelector('section.bestseller')
+    const promocao_geral = document.querySelector('section.sale')
+    console.log(item)
+    switch (item) {
+        case fone:
+            console.log("fone")
+            break;
+        case celular_promocao:
+            console.log("celular_promocao")
+            break;
+        case mais_vendidos:
+            console.log("mais_vendidos")
+            break;
+        case promocao_geral:
+            console.log("promocao_geral")
+            break;
+        default:
+            break;
+    }
 }
