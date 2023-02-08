c719d135eecdea8d8820c4e34ccaadffee94d5d4
IsmaelMartins934 <ismaelmartins934@gmail.com>
2023-02-08 11:37:33 -0300

    tv feito

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
 var btn = document.querySelectorAll('button')
+    document.addEventListener('click', (e) => {
+        e.preventDefault()
+        let takelittle = e.target
+        let parent = takelittle.closest('div')
+        console.log(parent)
+        if(parent.classList == 'saleButton'){
+            console.log('botao de promoçao')
+        }
+        if(takelittle.id == 'a1'){
+            window.location.href = 'http://127.0.0.1:5500/index.html'
+        }
+        if(takelittle.id == 'a2'){
+            window.location.href = 'html/produtos.html'
+        }
 
-for(i in btn){
-    btn[i].addEventListener('click', (e) => {
-        e.this
-        const path = e.composedPath()
-        page(path[1])
     })
-}
-
-function page(item){
-    const fone = document.querySelector('div.texto-fone')
-    const celular_promocao = document.querySelector('div.texto-prom')
-    const mais_vendidos = document.querySelector('section.bestseller')
-    const promocao_geral = document.querySelector('section.sale')
-    console.log(item)
-    switch (item) {
-        case fone:
-            console.log("fone")
-            break;
-        case celular_promocao:
-            console.log("celular_promocao")
-            break;
-        case mais_vendidos:
-            console.log("mais_vendidos")
-            break;
-        case promocao_geral:
-            console.log("promocao_geral")
-            break;
-        default:
-            break;
-    }
-}
