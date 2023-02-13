33818c49c7af721e2def4ae8aa13828929d08838
IsmaelMartins934 <ismaelmartins934@gmail.com>
2023-02-12 21:38:03 -0300

    fazendo js

--- js/takeIten.js
+++ js/takeIten.js
@@ -1,12 +1,19 @@
 document.addEventListener('click', (e) => {
+
+    let message = document.getElementsByClassName('message')
+
     e.preventDefault()
     let takeElement = e.target
     let parent = takeElement.closest('div')
-    if(parent.classList == 'links'){
+    if (takeElement.id == 'ini') {
+        window.location.href = '../index.html'
+        return
+    } if (takeElement.id == 'prod') {
+        
         return
     }
     localStorage.setItem('buyPage', parent.innerHTML)
     window.location.href = '../html/buyPage.html'
-    
+
 })
 
