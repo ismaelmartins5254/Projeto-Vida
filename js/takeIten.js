d9918f893571963c07b83b7bcb5310c5bac54f0f
IsmaelMartins934 <ismaelmartins934@gmail.com>
2023-02-15 19:36:20 -0300

    pag reload checked

--- js/takeIten.js
+++ js/takeIten.js
@@ -1,6 +1,9 @@
 document.addEventListener('click', (e) => {
 
-    let message = document.getElementsByClassName('message')
+    let loading = document.getElementById('loading')
+    let message = document.getElementById('men')
+    let me = document.createElement('span')
+    me.innerHTML = ''
 
     e.preventDefault()
     let takeElement = e.target
@@ -8,12 +11,33 @@ document.addEventListener('click', (e) => {
     if (takeElement.id == 'ini') {
         window.location.href = '../index.html'
         return
+    } if (takeElement.id == 'info') {
+        me.innerHTML = 'Página de informações está indisponível'
+        me.classList.add('fail')
+        message.appendChild(me)
+        setTimeout(() => {
+            me.style.display = 'none'
+        }, 2000)
+        return
     } if (takeElement.id == 'prod') {
+        me.innerHTML = 'Você já está na página de Produtos :)'
+        me.classList.add('fail')
+        message.appendChild(me)
         
+        setTimeout(() => {
+            me.style.display = 'none'
+        }, 2000)
         return
     }
+    let produtos = document.getElementById('produtos')
     localStorage.setItem('buyPage', parent.innerHTML)
-    window.location.href = '../html/buyPage.html'
+    let img = document.createElement('img')
+    produtos.style.display = 'none'
+    img.src = '../imagens/loading.svg'
+    loading.appendChild(img)
+    setTimeout(() => {
+        window.location.href = '../html/buyPage.html'
+
+    }, 2000)
 
 })
-
