20fb44e857fc05182b4c00e8b3559bffbb99fe40
izael <izaelgomes127@gmail.com>
2023-03-16 20:24:47 -0300

    bug carrinho resolvido

--- js/carPage.js
+++ js/carPage.js
@@ -1,3 +1,7 @@
+let infor = document.getElementById('info') //quando tiver na tela de pc
+let info = document.getElementById('a3')
+let message = document.getElementById('men')
+
 function onload(){
     let all = document.getElementById('all')
     let item = localStorage.getItem('carPage')
@@ -5,3 +9,29 @@ function onload(){
     all.innerHTML = item
 
 }
+
+info.addEventListener('click', (e) => {
+    let me = document.createElement('span')
+    e.preventDefault()
+    me.innerHTML = 'Página de informações está indisponível &#128531;'
+    me.classList.add('fail')
+    message.appendChild(me)
+    setTimeout(() => {
+        me.style.display = 'none'
+        me.remove()
+    }, 2000)
+    return
+})
+
+infor.addEventListener('click', (e) => { 
+    e.preventDefault()
+    let me = document.createElement('span')
+    me.innerHTML = 'Página de informações está indisponível &#128531;'
+    me.classList.add('fail')
+    message.appendChild(me)
+    setTimeout(() => {
+        me.style.display = 'none'
+        me.remove()
+    }, 2000)
+    return
+})
